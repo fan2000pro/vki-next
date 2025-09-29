53fad53d2a244e059e618f1814e3675be23e0926
Fokser <kulikovia@mer.ci.nsu.ru>
2025-09-29 14:08:17 +0700

    1234

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import TanStackQuery from '@/containers/TanStackQuery';
 import queryClient from '@/api/reactQueryClient';
 import { getGroupsApi } from '@/api/groupsApi';
 import type GroupInterface from '@/types/GroupInterface';
-import { getStudentsApi } from '@/api/studentsApi';
-import type StudentInterface from '@/types/StudentInterface';
 import Header from '@/components/layout/Header/Header';
 import Footer from '@/components/layout/Footer/Footer';
 import Main from '@/components/layout/Main/Main';
@@ -13,16 +11,19 @@ import Main from '@/components/layout/Main/Main';
 import type { Metadata } from 'next';
 
 import '@/styles/globals.scss';
+import { META_DESCRIPTION, META_TITLE } from '@/constants/meta';
+import type StudentInterface from '@/types/StudentInterface';
+import { getStudentsApi } from '@/api/studentsApi';
 
 export const metadata: Metadata = {
-  title: 'Вэб разработка ВКИ - Next.js шаблон',
-  description: 'Шаблон для веб-разработки с использованием Next.js, React Hook Form, Yup, SCSS, Eslint, TanStack Query (React Query)',
+  title: META_TITLE,
+  description: META_DESCRIPTION,
 };
 
 const RootLayout = async ({ children }: Readonly<{ children: React.ReactNode }>): Promise<React.ReactElement> => {
-  let groups: GroupInterface[];
 
   // выполняется на сервере - загрузка групп
+  let groups: GroupInterface[];
   await queryClient.prefetchQuery({
     queryKey: ['groups'],
     queryFn: async () => {
@@ -32,9 +33,8 @@ const RootLayout = async ({ children }: Readonly<{ children: React.ReactNode }>)
     },
   });
 
+  // выполняется на сервере - загрузка студентов
   let students: StudentInterface[];
-
-  // выполняется на сервере - загрузка групп
   await queryClient.prefetchQuery({
     queryKey: ['students'],
     queryFn: async () => {
